42c578a155c9f34b3238f4913d1d5f3128c1fcb9
S22102001 <Shahar.girtler@gmail.com>
2025-07-21 03:26:36 +0300

    Middleware
    logger validation and error handler

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+const fs = require('fs').promises;
+const logger = require('./logger');
+const validateTask = require('./validateTask');
+const errorHandler = require('./errorHandler');
+
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(logger);
+app.use(cors());
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', validateTask, async (req, res, next) => {
     const newTask = {
         id: uuidv4(),
         title: req.body.title.trim(),
@@ -14,12 +23,19 @@ app.post('/tasks', (req, res) => {
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
     };
-    console.log(newTask);
-    res.status(201).json(newTask);
+
+    try {
+        const data = await fs.readFile('tasks.json', 'utf-8');
+        const tasks = JSON.parse(data);
+        tasks.push(newTask);
+        await fs.writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+        res.status(201).json(newTask);
+    } catch (err) {
+        next(err);
+    }
 });
 
+app.use(errorHandler);
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-
