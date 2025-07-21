05b199c2c47183d64dd11a125967c747f62308df
S22102001 <Shahar.girtler@gmail.com>
2025-07-21 03:44:21 +0300

    data persistence
    All tasks live in the server/tasks.json file as a JSON array.

--- server/index.js
+++ server/index.js
@@ -6,9 +6,24 @@ const logger = require('./logger');
 const validateTask = require('./validateTask');
 const errorHandler = require('./errorHandler');
 
-
 const app = express();
 const PORT = 3000;
+
+const DATA_PATH = './tasks.json';
+
+async function readTasks() {
+    try {
+        const data = await fs.readFile(DATA_PATH, 'utf-8');
+        return JSON.parse(data);
+    }
+    catch {
+        return [];
+    }
+}
+
+async function writeTasks(TasksArray) {
+    await fs.writeFile(DATA_PATH, JSON.stringify(TasksArray, null, 2));
+}
 
 app.use(express.json());
 app.use(logger);
@@ -25,11 +40,19 @@ app.post('/tasks', validateTask, async (req, res, next) => {
     };
 
     try {
-        const data = await fs.readFile('tasks.json', 'utf-8');
-        const tasks = JSON.parse(data);
+        const tasks = await readTasks();
         tasks.push(newTask);
-        await fs.writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+        await writeTasks(tasks);
         res.status(201).json(newTask);
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('/tasks', async (req, res, next) => {
+    try {
+        const tasks = await readTasks();
+        res.json(tasks);
     } catch (err) {
         next(err);
     }
